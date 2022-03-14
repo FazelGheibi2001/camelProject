ebe1551be4a284a1276de73fbf5f28e08625005d
Fazel <fazelgheibi2001@gmail.com>
2022-03-14 19:17:14 +0330

    init project

---
+++ src/main/java/com/camel/camel/FirstRoute.java
@@ -0,0 +1,13 @@
+package com.camel.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
